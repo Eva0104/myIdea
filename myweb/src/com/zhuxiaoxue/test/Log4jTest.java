22a72af013b4e0eb56340c236260e1b3162d51d2
Eva0104 <zhuxiaoxue0104@126.com>
2016-06-13 16:28:13 +0800

    日志组件(logback)的使用

--- myweb/src/com/zhuxiaoxue/test/Log4jTest.java
+++ myweb/src/com/zhuxiaoxue/test/Log4jTest.java
@@ -1,18 +1,19 @@
 package com.zhuxiaoxue.test;
-
-import org.apache.log4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.junit.Test;
 
 public class Log4jTest {
     @Test
     public void testLog4j() {
-        Logger logger = Logger.getLogger(Log4jTest.class);
+        Logger logger = LoggerFactory.getLogger(Log4jTest.class);
 
+        String name = "tom";
+        String bookName="红";
         logger.trace("trance message");
         logger.debug("debug message");
-        logger.info("info message");
+        logger.info("{}借阅了{}",name,bookName);
         logger.warn("warn message");
         logger.error("error message");
-        logger.fatal("fatal message");
     }
 }
