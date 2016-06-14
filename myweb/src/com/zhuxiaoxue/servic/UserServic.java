83af6cab1220f3bcce38489ea1c7f25d6a02c4ca
Eva0104 <zhuxiaoxue0104@126.com>
2016-06-14 17:13:49 +0800

    添加文档注释

--- myweb/src/com/zhuxiaoxue/servic/UserServic.java
+++ myweb/src/com/zhuxiaoxue/servic/UserServic.java
@@ -12,12 +12,19 @@ public class UserServic {
     private String salt = "%%%%%%%%%%";
     Logger logger = LoggerFactory.getLogger(UserServic.class);
 
+    /**
+     * 用户登录
+     *
+     * @param username 用户名
+     * @param password 密码
+     * @return 如果登录成功返回User对象，登录失败则返回null
+     */
     public User login(String username, String password) {
         User user = dao.queryByUsername(username);
         DigestUtils.md5Hex(password + salt);
-        if (user != null && user.getPassword() .equals(DigestUtils.md5Hex(password + salt))) {
+        if (user != null && user.getPassword().equals(DigestUtils.md5Hex(password + salt))) {
             return user;
-        }else{
+        } else {
 
             return null;
         }
