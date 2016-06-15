4b5c0570e3f74af48de91bb8601aa9c93583827b
Eva0104 <zhuxiaoxue0104@126.com>
2016-06-15 10:06:05 +0800

    练习发送Html 和带附件的邮件

--- myweb/src/com/zhuxiaoxue/test/MailTest.java
+++ myweb/src/com/zhuxiaoxue/test/MailTest.java
@@ -3,9 +3,7 @@ package com.zhuxiaoxue.test;
 import com.zhuxiaoxue.entity.User;
 import com.zhuxiaoxue.util.Config;
 import com.zhuxiaoxue.util.MailUtil;
-import org.apache.commons.mail.Email;
-import org.apache.commons.mail.EmailException;
-import org.apache.commons.mail.SimpleEmail;
+import org.apache.commons.mail.*;
 import org.joda.time.DateTime;
 import org.junit.Test;
 
@@ -19,23 +17,63 @@ public class MailTest {
         email.setSmtpPort(Integer.parseInt(Config.getConfig("mail.port")));
         email.setCharset(Config.getConfig("mail.charset"));
 
-        try{
+        try {
             email.setFrom(Config.getConfig("mail.setFrom"));
             email.setSubject("练习一下 ^-^");
-            email.setMsg("现在时间"+ DateTime.now().toString("yyyy-MM-dd hh:mm:ss"));
+            email.setMsg("现在时间" + DateTime.now().toString("yyyy-MM-dd hh:mm:ss"));
             //email.addTo("342030831@qq.com");
             email.addTo("xieyue86@163.com");
             email.send();
-        }catch (EmailException ex){
+        } catch (EmailException ex) {
             throw new RuntimeException(ex);
         }
 
     }
+
     @Test
-    public  void mailUtilTest(){
-        User user = new User();
-        user.setName("Tom");
-        user.setAddress("zhuxiaoxue0104@126.com");
-        MailUtil mailUtil = new MailUtil();
+    public void sendHtmlEmailTest() {
+        HtmlEmail email = new HtmlEmail();
+        email.setAuthentication(Config.getConfig("mail.username"), Config.getConfig("mail.password"));
+        email.setHostName(Config.getConfig("mail.hostName"));
+        email.setSmtpPort(Integer.parseInt(Config.getConfig("mail.port")));
+        email.setCharset(Config.getConfig("mail.charset"));
+
+        try {
+            email.setFrom(Config.getConfig("mail.setFrom"));
+            email.setSubject("练习一下 ^-^");
+            email.setHtmlMsg("<h1 style=\"color:red\">练习发送Html邮件</h1>");
+            //email.addTo("342030831@qq.com");
+            email.addTo("zhuxiaoxue0104@126.com");
+            email.send();
+        } catch (EmailException ex) {
+            throw new RuntimeException(ex);
+        }
     }
+
+    @Test
+    public void sendAttEmailTest() {
+        EmailAttachment ea = new EmailAttachment();
+        ea.setPath("D:/朱小雪/0U00M3B-110.jpg");
+
+        MultiPartEmail ml = new MultiPartEmail();
+
+        ml.setAuthentication(Config.getConfig("mail.username"), Config.getConfig("mail.password"));
+        ml.setHostName(Config.getConfig("mail.hostName"));
+        ml.setSmtpPort(Integer.parseInt(Config.getConfig("mail.port")));
+        ml.setCharset(Config.getConfig("mail.charset"));
+
+        try{
+            ml.setFrom(Config.getConfig("mail.setFrom"));
+            ml.setSubject("练习一下 ^-^");
+            ml.setMsg("发送带附件的邮件");
+            ml.addTo("zhuxiaoxue0104@126.com");
+            ml.attach(ea);
+            ml.send();
+        }catch (Exception ex){
+            throw new RuntimeException(ex);
+        }
+
+
+    }
+
 }
