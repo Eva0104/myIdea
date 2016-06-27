65d85cada77833cc43f5f116c51690908c3fc3ad
Eva0104 <zhuxiaoxue0104@126.com>
2016-06-27 18:06:23 +0800

    Mybatis

---
+++ myBatis/src/test/java/com/zhuxiaoxue/test.java
@@ -0,0 +1,108 @@
+package com.zhuxiaoxue;
+
+import com.zhuxiaoxue.pojo.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.List;
+
+public class test {
+
+    @Test
+    public void testFindById(){
+        try {
+            Reader reader = Resources.getResourceAsReader("mybatis.xml");
+            SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+            SqlSession sqlSession = sqlSessionFactory.openSession();
+
+            User user = sqlSession.selectOne("com.zhuxiaoxue.mapper.userMapper.findById",1);
+
+            sqlSession.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testFindAll(){
+        try {
+            Reader reader = Resources.getResourceAsReader("mybatis.xml");
+            SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+            SqlSession sqlSession = sqlSessionFactory.openSession();
+
+            List<User> userList = sqlSession.selectList("com.zhuxiaoxue.mapper.userMapper.findAll");
+            for(User u : userList){
+                System.out.println(u);
+            }
+            sqlSession.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Test
+    public void testSave(){
+        try {
+            Reader reader = Resources.getResourceAsReader("mybatis.xml");
+            SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+            SqlSession sqlSession = sqlSessionFactory.openSession();
+
+            User user = new User();
+            user.setName("小李子");
+            user.setPassword("123456");
+            user.setAddress("开封");
+
+            sqlSession.insert("com.zhuxiaoxue.mapper.userMapper.save",user);
+
+            sqlSession.commit();
+            sqlSession.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testUpdate(){
+        try {
+            Reader reader = Resources.getResourceAsReader("mybatis.xml");
+            SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+            SqlSession sqlSession = sqlSessionFactory.openSession();
+
+            User user = sqlSession.selectOne("com.zhuxiaoxue.mapper.userMapper.findById",4);
+
+            user.setName("大白");
+            user.setPassword("111111");
+            user.setAddress("开封");
+
+            sqlSession.update("com.zhuxiaoxue.mapper.userMapper.update",user);
+
+            sqlSession.commit();
+            sqlSession.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testDelete(){
+        try {
+            Reader reader = Resources.getResourceAsReader("mybatis.xml");
+            SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+            SqlSession sqlSession = sqlSessionFactory.openSession();
+
+            sqlSession.delete("com.zhuxiaoxue.mapper.userMapper.delete",14);
+
+            sqlSession.commit();
+            sqlSession.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
