5fc8483622d0f0c80a7686ef977e5f80294641f3
Eva0104 <zhuxiaoxue0104@126.com>
2016-06-28 17:40:21 +0800

    OGNL(对象图导航语言)

---
+++ myBatis/src/main/java/com/zhuxiaoxue/mapper/ResultMapper.java
@@ -0,0 +1,7 @@
+package com.zhuxiaoxue.mapper;
+
+/**
+ * Created by Eric on 2016/6/28.
+ */
+public interface ResultMapper {
+}
---
+++ myBatis/src/test/java/com/zhuxiaoxue/ResultMapperTest.java
@@ -0,0 +1,4 @@
+package com.zhuxiaoxue;
+
+public class ResultMapperTest {
+}
