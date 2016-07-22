e3ae5886ac63dd74112b38340d7a937dd9c26a34
Eva0104 <zhuxiaoxue0104@126.com>
2016-07-22 20:58:26 +0800

    serializer

---
+++ reids/src/main/java/com/zhuxiaoxue/pojo/User.java
@@ -0,0 +1,50 @@
+package com.zhuxiaoxue.pojo;
+
+public class User {
+
+    private Integer id;
+    private String name;
+    private Float score;
+
+    public User() {
+    }
+
+    public User(Integer id, String name, Float score) {
+        this.id = id;
+        this.name = name;
+        this.score = score;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", score=" + score +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Float getScore() {
+        return score;
+    }
+
+    public void setScore(Float score) {
+        this.score = score;
+    }
+}
