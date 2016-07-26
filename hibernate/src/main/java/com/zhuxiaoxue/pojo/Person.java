8bf363c9798f6ec21b4e8e46a4fbdbd24f127ea6
Eva0104 <zhuxiaoxue0104@126.com>
2016-07-26 16:55:10 +0800

    一对一

---
+++ hibernate/src/main/java/com/zhuxiaoxue/pojo/Person.java
@@ -0,0 +1,33 @@
+package com.zhuxiaoxue.pojo;
+
+public class Person {
+
+    private Integer id;
+    private String name;
+
+    private Card card;
+
+    public Card getCard() {
+        return card;
+    }
+
+    public void setCard(Card card) {
+        this.card = card;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ hibernate/src/test/java/com/zhuxiaoxue/test/OneToOneTestCase.java
@@ -0,0 +1,78 @@
+package com.zhuxiaoxue.test;
+
+import com.zhuxiaoxue.pojo.Card;
+import com.zhuxiaoxue.pojo.Person;
+import com.zhuxiaoxue.util.HibernateUtil;
+import org.hibernate.Session;
+import org.junit.Test;
+
+public class OneToOneTestCase {
+
+    /*
+     最佳实践：先存主，再存从；
+     */
+
+    @Test
+    public void testSave(){
+        Session session = HibernateUtil.getSession();
+        session.beginTransaction();
+
+        Person person = new Person();
+        person.setName("jim");
+
+        Card card = new Card();
+        card.setCardname("101");
+        card.setPerson(person);
+
+        session.save(person);
+        session.save(card);
+
+        session.getTransaction().commit();
+    }
+
+    @Test
+    public void testFindPerson(){
+        Session session = HibernateUtil.getSession();
+        session.beginTransaction();
+
+        Person person = (Person) session.get(Person.class,14);
+
+        System.out.println(person.getName() +"--->"+ person.getCard().getCardname());
+
+        session.getTransaction().commit();
+    }
+
+    @Test
+    public void testFindCard(){
+        Session session = HibernateUtil.getSession();
+        session.beginTransaction();
+
+        Card card = (Card) session.get(Card.class,14);
+
+        System.out.println(card.getCardname() +"--->"+ card.getPerson().getName());
+
+        session.getTransaction().commit();
+    }
+
+    @Test
+    public void testDelCard(){
+        Session session = HibernateUtil.getSession();
+        session.beginTransaction();
+
+        Card card = (Card) session.get(Card.class,14);
+        session.delete(card);
+
+        session.getTransaction().commit();
+    }
+
+    @Test
+    public void testDelPerson(){
+        Session session = HibernateUtil.getSession();
+        session.beginTransaction();
+
+        Person person = (Person) session.get(Person.class,15);
+        session.delete(person);
+
+        session.getTransaction().commit();
+    }
+}
